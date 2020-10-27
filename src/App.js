b4ff4804daade301dad786311e309cb1efc67e1f
Heena <heena.dhingra@hyperdart.com>
2020-10-27 17:05:22 +0530

    autocomplete functionality completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import TextField from "@material-ui/core/TextField";
+import Autocomplete from "@material-ui/lab/Autocomplete";
+import { useState } from "react";
 
 function App() {
+  const [fruit, setFruit] = useState("");
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Autocomplete
+        id="combo-box-demo"
+        options={arr}
+        getOptionLabel={option => option}
+        value={fruit}
+        onChange={(event, value) => {
+          setFruit(value);
+        }}
+        renderInput={params => (
+          <TextField
+            {...params}
+            label="Search fruit name"
+            variant="outlined"
+            fullWidth
+          />
+        )}
+      />
+
+      {fruit && <h6>You selected {fruit}</h6>}
     </div>
   );
 }
+
+const arr = ["Apple", "Mango", "Kiwi", "Strawberry", "Melon", "Custard Apple"];
 
 export default App;
